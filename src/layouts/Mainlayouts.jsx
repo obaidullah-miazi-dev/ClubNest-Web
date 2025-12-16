2f762f2a0eefdfe8520a8bce8a63ec5cc5dc752c
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-12-16 22:14:32 +0600

    scroll to top component added

--- src/layouts/Mainlayouts.jsx
+++ src/layouts/Mainlayouts.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../components/shared/Navbar';
 import Footer from '../components/shared/Footer';
+import ScrollToTop from '../components/ScrollToTop';
 
 const Mainlayouts = () => {
     return (
         <div>
+            <ScrollToTop />
             <Navbar />
             <Outlet />
             <Footer />
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router'
-import { router } from './routes/Router.jsx'
-import AuthProvider from './provider/authProvider.jsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-const queryClient = new QueryClient()
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import { router } from "./routes/Router.jsx";
+import AuthProvider from "./provider/authProvider.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-    <RouterProvider router={router}/>
-   </AuthProvider>
-   </QueryClientProvider>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
